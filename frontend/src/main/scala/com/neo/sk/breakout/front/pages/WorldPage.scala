22bd09a34f65c0b496ebede24c1902b1e945ddaa
zhaoyin <zhaoyin@neotel.com.cn>
2019-02-24 20:32:39 +0800

    world页面优化

--- frontend/src/main/scala/com/neo/sk/breakout/front/pages/WorldPage.scala
+++ frontend/src/main/scala/com/neo/sk/breakout/front/pages/WorldPage.scala
@@ -27,26 +27,42 @@ class WorldPage(identity:String,playerName:String,playerType:Byte) extends Page 
   var roomInuse =  Var(List.empty[(Long,(String,Int,List[String]))])
 
   var chooseRoomId = -1l
+  var chooseRoomIdVar = Var(-1l)
   var roomPlayerNum = 1
   var roomType = 0 //1:合作  2：竞争
   var roomTypeVar = Var(0)
   var roomName = None
 
   var roomLists:Rx[Node] = roomInuse.map(roomList=>
-    <div class="roomContain">
-      {roomList.map(room =>
-      <div class="roomStyle" onclick={()=> clickRoom(room)}>
-        <div class="roomName">{room._2._1}</div>
-        {roomTypePic(room._2._2)}
-        <div style="color:#858585;line-height: 30px;">{room._2._3.length}/2</div>
+    {
+      def check(room:(Long,(String,Int,List[String]))) = {
+        <div>{chooseRoomIdVar.map(id => if(id==room._1){
+          <div class="roomStyleC" onclick={()=> clickRoom(room)}>
+            <div class="roomName">{room._2._1}</div>
+            {roomTypePic(room._2._2)}
+            <div style="color:#858585;line-height: 30px;">{room._2._3.length}/2</div>
+          </div>
+        }else{
+          <div class="roomStyle" onclick={()=> clickRoom(room)}>
+            <div class="roomName">{room._2._1}</div>
+            {roomTypePic(room._2._2)}
+            <div style="color:#858585;line-height: 30px;">{room._2._3.length}/2</div>
+          </div>
+        })}</div>
+
+      }
+
+      <div class="roomContain">
+        {roomList.map(check(_))}
       </div>
-    )}
-    </div>
+    }
+
   )
 
   def clickRoom(room:(Long,(String,Int,List[String]))) ={
     chooseRoomId = room._1
     roomPlayerNum = room._2._3.length
+    chooseRoomIdVar := room._1
   }
   def roomTypePic(types:Int) = {
     if(types==1){
@@ -102,17 +118,20 @@ class WorldPage(identity:String,playerName:String,playerType:Byte) extends Page 
     <div class ="world">
       <div class="two">
         <div class="chooseWorld">
-          <div style="font-size:25px">选择房间</div>
-          <div onclick={()=>init()}>刷新</div>
+          <div style="display:flex;justify-content:space-between;padding-right:80px">
+            <div style="font-size:25px">选择房间</div>
+            <img onclick={()=>init()} src="/breakout/static/img/refresh.png" class="refresh"></img>
+          </div>
           {roomLists}
           <div class="joinButton" onclick={() => joinWorld}>加入</div>
         </div>
         <div class="createWorld">
           <div style="font-size:25px">创建房间</div>
-          <div style="margin-top:70px">
+          <div style="margin-top:40px;height:70%">
             <img src="/breakout/static/img/touxiang.png" style="width:80px;height:80px;margin:0 auto;display:block"></img>
             <div style="margin:10px auto;font-size:20px;text-align:center">{playerName}</div>
             <input id="roomName" placeholder="请输入您的房间名" class="inputStyle" style="margin:15px 0;height:40px;font-size:18px"></input>
+            <div>选择房间类型（合作/竞争）：</div>
             <div class="worldTypes">
               {cooperation}
               {compete}
